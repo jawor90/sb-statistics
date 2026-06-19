d03441afba34bfc967c1b7fa18424b5d3fb56b89
Kamil Jaworowski <jaworowski.kamil@gmail.com>
2026-06-19 16:36:31 +0200

    Add missing validation

--- src/tracking/create-event.dto.ts
+++ src/tracking/create-event.dto.ts
@@ -1,8 +1,24 @@
+import { IsObject, IsOptional, IsString } from 'class-validator';
+
 export class CreateEventDto {
+  @IsString()
   eventType: string;
+
+  @IsString()
   contentId: string;
+
+  @IsString()
   sessionId: string;
+
+  @IsOptional()
+  @IsString()
   userId?: string;
+
+  @IsOptional()
+  @IsObject()
   metadata?: Record<string, unknown>;
+
+  @IsOptional()
+  @IsString()
   platform?: string;
 }
